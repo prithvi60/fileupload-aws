6de78d34f4bb6b8c4fe2462b432f76bc1c71176c
Prithvi <prithvin19@gmail.com>
2023-11-15 14:14:13 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -26,47 +26,52 @@ function App() {
         identityPoolId: "eu-north-1:6882a53f-ea7c-49cb-b0b6-bea5052ec264",
       }),
     });
-    const command = new ListObjectsCommand({ Bucket: "theprintguy-customerfiles" ,ExpectedBucketOwner:"200994887321"});
+    const command = new ListObjectsCommand({
+      Bucket: "theprintguy-customerfiles",
+      ExpectedBucketOwner: "200994887321",
+    });
     client.send(command).then(({ Contents }) => setObjects(Contents || []));
   }, []);
 
   return (
     <div className="App">
       <h2>Design Files from Customers</h2>
-       <table>
-      <thead>
-        <tr>
-          <th>File Name</th>
-          <th>Upload Date</th>
-          <th>File Size</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {objects.map((o) => {
-
-                  // Split the date string into an array of strings, using whitespace as the delimiter.
-const dateParts = String(o.LastModified).split(/\s+/);
-
-// Get the day, month, and year from the date parts.
-const day = dateParts[2];
-const month = dateParts[1];
-const year = dateParts[3];
-         return <tr key={o.ETag}>
-            <td>{o.Key}</td>
-            <td>{`${day} ${month} ${year}`}</td>
-            <td>{o.Size}Kb</td>
-            <td><button>
-            <a 
-        target="blank"
-        href={
-          `https://theprintguy-customerfiles.s3.ap-south-1.amazonaws.com/${o.Key}`
-        } >Download</a>
-              </button></td>
+      <table>
+        <thead>
+          <tr>
+            <th>File Name</th>
+            <th>Upload Date</th>
+            <th>File Size</th>
+            <th>Action</th>
           </tr>
-})}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {objects.map((o) => {
+            // Split the date string into an array of strings, using whitespace as the delimiter.
+            const dateParts = String(o.LastModified).split(/\s+/);
+
+            // Get the day, month, and year from the date parts.
+            const day = dateParts[2];
+            const month = dateParts[1];
+            const year = dateParts[3];
+            return (
+              <tr key={o.ETag}>
+                <td>{o.Key}</td>
+                <td>{`${day} ${month} ${year}`}</td>
+                <td>{(Number(o.Size)/1000).toFixed(1)}Mb</td>
+                <td>
+                  <a
+                    target="blank"
+                    href={`https://theprintguy-customerfiles.s3.ap-south-1.amazonaws.com/${o.Key}`}
+                  >
+                    <button>Download</button>
+                  </a>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
