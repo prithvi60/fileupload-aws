53b19d4fdae082d1ade7e27aa63d643e67c83bc2
Prithvi <prithvin19@gmail.com>
2023-12-16 21:23:02 +0530

    remove api key

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,3 @@
-/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// snippet-start:[javascript.v3.scenarios.web.ListObjects]
 import { useEffect, useState } from "react";
 import {
   ListObjectsCommand,
@@ -32,76 +26,7 @@ function App() {
     });
     client.send(command).then(({ Contents }) => setObjects(Contents || []));
   }, []);
-  // useEffect(()=>{
-  //   let tableContainer = document.getElementById('tableContainer');
-  //   if(tableContainer)
-  //   tableContainer.scrollLeft = tableContainer.scrollWidth;
-  // },[])
-  // const getCustomerInfo= async (id: string) => {
-  //   // console.log("test price deets",id)
-  //   const username ="fd829fb1b82ee954210c08ffa03c3746";
-  //   const password="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";
-  //   const response = await fetch(`https://2f8923-2.myshopify.com/admin/api/2023-10/customers/${id}.json`, {
-  //     method: "GET",
-  //     headers: {
-  //       "Authorization": `Basic ${btoa(username + ":" + password)}`,
-  //       "Cache-Control": "no-cache, no-store, must-revalidate"
-  //       },
-  //   });
-  
-  //   if (response.status === 200) {
-  //     return response.json()
-  //   } 
-  // };
 
-//   const [comments,setComments]=useState([])
-//   let urls = objects.sort((a, b) => {
-//     // @ts-ignore
-// return new Date(b.LastModified).getTime() - new Date(a.LastModified).getTime();
-// }).map((o) => `https://2f8923-2.myshopify.com/admin/api/2023-10/customers/7399818199359.json`);
-
-// useEffect(() => {
-//   fetchComments();
-// }, [])
-
-// const fetchComments = async () => {
-//   let fetchedComments:any = [];
-//   Promise.all(
-//     urls.map(async (url) => {
-//       const username ="fd829fb1b82ee954210c08ffa03c3746";
-//       const password="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";
-//       let response = await fetch(url, {
-//         method: "GET",
-//         headers: {
-//           "Authorization": `Basic ${btoa(username + ":" + password)}`,
-//           "Cache-Control": "no-cache, no-store, must-revalidate"
-//           },
-//       })
-//       let data = await response.json();
-//       fetchedComments.push(data);
-//       console.log(fetchedComments)
-//     })
-//   )
-//   setComments(fetchedComments);
-// }
-
-// const fetchComments = async () => {
-//   const username ="fd829fb1b82ee954210c08ffa03c3746";
-//   const password="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";
-//   const response = await Promise.all(
-//     urls.map((url) =>  fetch(url, {
-//       method: "GET",
-//       mode:"no-cors",
-//       headers: {
-//         "Authorization": `Basic ${btoa(username + ":" + password)}`,
-//         "Cache-Control": "no-cache, no-store, must-revalidate"
-//         },
-//     }).then(res => res.json()))
-//   )
-//   const fetchedComments = [].concat.apply([], response);
-//   setComments(fetchedComments);
-// }
-// console.log("customer data",comments)
   return (
     <div className="App">
       <h2>Design Files from Customers</h2>
